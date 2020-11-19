570308cd2b65eaacf818c0e12e56cc819b229e86
Rita Bogachova <margita@tut.by>
2020-11-19 12:46:51 -0800

    Updates

---
+++ precache-manifest.e02d6a38568275afcc17c286dc1429d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3009c3aaeb537242d8f26af9f06b0790",
+    "url": "/minesweeper-javascript/index.html"
+  },
+  {
+    "revision": "0b499d931a55f19feef7",
+    "url": "/minesweeper-javascript/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "727fc6eb89742fe21ea2",
+    "url": "/minesweeper-javascript/static/css/main.ce287147.chunk.css"
+  },
+  {
+    "revision": "0b499d931a55f19feef7",
+    "url": "/minesweeper-javascript/static/js/2.0fee0585.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/minesweeper-javascript/static/js/2.0fee0585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727fc6eb89742fe21ea2",
+    "url": "/minesweeper-javascript/static/js/main.a48f49eb.chunk.js"
+  },
+  {
+    "revision": "2e5912b3cc86448cf135",
+    "url": "/minesweeper-javascript/static/js/runtime-main.7fd66643.js"
+  }
+]);
